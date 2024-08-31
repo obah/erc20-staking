1c5c552b26f0dc82c8f489a17f6b7045c0a57b9a
obah <olusojiobah@gmail.com>
2024-08-31 16:12:51 +0100

    feat: implement abstract contract

---
+++ contracts/Stake.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IStake, StakeErrors, StakeEvents} from "./IStake.sol";
+
+abstract contract Stake is IStake, StakeErrors, StakeEvents {
+    /// @dev keeps track of each address's balance 
+    mapping(address => uint256) public balances;
+
+    struct StakeRecord {
+        uint256 amount;
+        uint256 timeStaked;
+        uint256 maturity;
+        bool withdrawn;
+        uint256 lastRewardTime;
+        uint256 reward;
+        uint256 depositedAmount;
+    }
+
+    uint256 counter;
+
+    /// @dev keeps track of each deposit 
+    mapping(address => StakeRecord[]) userStakes;
+
+    mapping(address => StakeRecord[]) availableUserStakes;
+
+    constructor() {}
+
+    function _getBalance(address account) public view  virtual returns(uint256) {
+        return balances[account];
+    }
+
+    function calculateReward(uint256 amount, uint256 duration) internal view returns(uint256) {
+        //yearSeconds = 60 * 60 * 24 * 7 * 52;
+        uint256 yearSeconds = 31449600;
+
+        uint256 timeInSeconds = block.timestamp - duration;
+        uint256 timeInYear = timeInSeconds / yearSeconds;  
+        uint256 prt = amount * 10 * timeInYear;
+
+        return prt / 100; 
+    }
+
+     function getAvailableStakes(address account) internal {
+        StakeRecord[] memory _userStakes = userStakes[account];
+
+        StakeRecord[] storage _availableStakes = availableUserStakes[msg.sender];
+
+        for(uint16 i; i< _userStakes.length; i++) {
+            if(!_userStakes[i].withdrawn) {
+                _availableStakes.push(_userStakes[i]);
+            }
+        }
+    }
+
+    function checkWithdrawableFunds(address account) internal view returns (uint256) {
+        StakeRecord[] memory _userStakes = availableUserStakes[account];
+
+        uint256 _withdrawableFunds;
+
+        for(uint16 i; i < _userStakes.length; i++) {
+            if(_userStakes[i].maturity >= block.timestamp) {
+                _withdrawableFunds = _withdrawableFunds + _userStakes[i].amount;
+            }
+        }
+
+        return _withdrawableFunds;
+    }
+
+    function _payRewards(address account) public {
+        if(balances[account] <= 0) {
+            revert NoActiveStake();
+        }
+
+        StakeRecord[] storage _userStakes = availableUserStakes[account];
+
+        uint256 _totalReward;
+
+        for (uint256 i = 0; i < _userStakes.length; i++) {
+            StakeRecord storage _stake = _userStakes[i];
+
+            uint256 _startTime = _stake.lastRewardTime > 0 ? _stake.lastRewardTime : _stake.timeStaked;
+
+            uint256 _duration = block.timestamp - _startTime;
+
+            uint256 _reward = calculateReward(_stake.amount, _duration);
+
+            _totalReward = _totalReward + _reward;
+            _stake.reward = _stake.reward + _totalReward;
+            _stake.amount = _stake.amount + _totalReward;
+            balances[account] = balances[account] + _totalReward;
+
+            _stake.lastRewardTime = block.timestamp;
+        }
+
+        emit RewardPaid(account, _totalReward);
+    }
+}
--- contracts/StakingETH.sol
+++ contracts/StakingETH.sol
@@ -2,44 +2,17 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {IStake, StakeErrors, StakeEvents} from "./IStake.sol";
+import "./Stake.sol";
 
 
-contract EthStaking is ReentrancyGuard {
-    error AddressZero();
-    error InvalidDeposit();
-    error NoActiveStake();
-    error InsufficientFunds();
-    error ImmatureStake();
-
-    /// @dev keeps track of each address's balance 
-    mapping(address => uint256) balances;
-
-    struct Stake {
-        uint256 amount;
-        uint256 timeStaked;
-        uint256 maturity;
-        bool withdrawn;
-        uint256 lastRewardTime;
-        uint256 reward;
-        uint256 depositedAmount;
-    }
-
-    uint256 counter;
-
-    /// @dev keeps track of each deposit 
-    mapping(address => Stake[]) userStakes;
-
-    mapping(address => Stake[]) availableUserStakes;
-
+contract EthStaking is ReentrancyGuard, IStake, StakeErrors, StakeEvents, Stake {
+   
     constructor() {}
 
     receive() external payable {}
 
-    function getBalance() external view returns (uint256) {
-        return balances[msg.sender];
-    }
-
-    function depositETH(uint256 _duration) external payable nonReentrant{
+    function depositETH(uint256 _duration) external payable nonReentrant {
          if(msg.sender == address(0)) {
             revert AddressZero();
         }
@@ -48,14 +21,13 @@ contract EthStaking is ReentrancyGuard {
             revert InvalidDeposit();
         }
 
-        balances[msg.sender] = balances[msg.sender] + msg.value;
+        uint256 _balance = _getBalance(msg.sender);
 
-        Stake storage _stake = userStakes[msg.sender][counter];
+        _balance = _balance + msg.value;
 
-        //_secsInDay = 60 * 60 * 24
-        uint256 _secsInDay = 86400;
+        StakeRecord storage _stake = userStakes[msg.sender][counter];
 
-        uint256 _durationInSecs = _duration / _secsInDay; 
+        uint256 _durationInSecs = _duration / 1 days; 
 
         _stake.amount = msg.value;
         _stake.depositedAmount = msg.value;
@@ -65,92 +37,29 @@ contract EthStaking is ReentrancyGuard {
         counter = counter + 1;
     }
 
-    function calculateReward(uint256 _amount, uint256 _duration) private view returns(uint256) {
-        //_yearSeconds = 60 * 60 * 24 * 7 * 52;
-        uint256 _yearSeconds = 31449600;
-
-        uint256 _timeInSeconds = block.timestamp - _duration;
-        uint256 _timeInYear = _timeInSeconds / _yearSeconds;  
-        uint256 _prt = _amount * 10 * _timeInYear;
-
-        return _prt / 100; 
-    }
-
-    function getAvailableStakes() private {
-        Stake[] memory _userStakes = userStakes[msg.sender];
-        Stake[] storage _availableStakes = availableUserStakes[msg.sender];
-
-        for(uint16 i; i< _userStakes.length; i++) {
-            if(!_userStakes[i].withdrawn) {
-                _availableStakes.push(_userStakes[i]);
-            }
-        }
-    }
-
-    function checkWithdrawableFunds() private view returns (uint256) {
-        Stake[] memory _userStakes = availableUserStakes[msg.sender];
-
-        uint256 _withdrawableFunds;
-
-        for(uint16 i; i < _userStakes.length; i++) {
-            if(_userStakes[i].maturity >= block.timestamp) {
-                _withdrawableFunds = _withdrawableFunds + _userStakes[i].amount;
-            }
-        }
-
-        return _withdrawableFunds;
-    }  
-
-    function payRewards() public {
-        if(balances[msg.sender] <= 0) {
-            revert NoActiveStake();
-        }
-
-        Stake[] storage _userStakes = availableUserStakes[msg.sender];
-
-        uint256 _totalReward;
-
-        for (uint256 i = 0; i < _userStakes.length; i++) {
-            Stake storage _stake = _userStakes[i];
-
-            uint256 _startTime = _stake.lastRewardTime > 0 ? _stake.lastRewardTime : _stake.timeStaked;
-
-            uint256 _duration = block.timestamp - _startTime;
-
-            uint256 _reward = calculateReward(_stake.amount, _duration);
-
-            _totalReward = _totalReward + _reward;
-            _stake.reward = _stake.reward + _totalReward;
-            _stake.amount = _stake.amount + _totalReward;
-            balances[msg.sender] = balances[msg.sender] + _totalReward;
-
-            _stake.lastRewardTime = block.timestamp;
-        }
-    }
-
     function withdraw(uint256 _amount) external nonReentrant {
         if(msg.sender == address(0)) {
             revert AddressZero();
         }
 
-        payRewards();
+        _payRewards(msg.sender);
 
         if(_amount < balances[msg.sender]) {
             revert InsufficientFunds();
         }
 
-        uint256 _withdrawableFunds = checkWithdrawableFunds();
+        uint256 _withdrawableFunds = checkWithdrawableFunds(msg.sender);
 
         if(_withdrawableFunds <= 0) {
             revert ImmatureStake();
         }
 
-        Stake[] storage _userStakes = availableUserStakes[msg.sender];
+        StakeRecord[] storage _userStakes = availableUserStakes[msg.sender];
 
         uint256 _remainingAmount = _amount;
 
         for (uint256 i = 0; i < _userStakes.length; i++) {
-            Stake storage _stake = _userStakes[i];
+            StakeRecord storage _stake = _userStakes[i];
             if (_stake.amount > 0) {
                 if (_stake.amount >= _remainingAmount) { 
                     _stake.amount = _stake.amount - _remainingAmount;
